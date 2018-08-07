2a8ae04bdbddb5322b8d8b09126edb1f7b0437fb
Craig Eckert <32557342+Eckertc@users.noreply.github.com>
2018-08-06 21:03:22 -0400

    Add files via upload

--- DrawGiftWrapping.py
+++ DrawGiftWrapping.py
@@ -10,17 +10,21 @@ class Example(tk.Frame):
         self.PointList = PointList
         self.initUI()
 
-
     def initUI(self):
         self.master.title("Drawing Convex Hull")
         self.pack(fill=tk.BOTH, expand=1)
         canvas = tk.Canvas(self)
+
+        # draw axis
+        canvas.create_line(0, MAX_HEIGHT/2, MAX_WIDTH, MAX_HEIGHT/2, fill = "#b2d8af")
+        canvas.create_line(MAX_WIDTH/2, 0, MAX_WIDTH/2, MAX_HEIGHT, fill = "#b2d8af")
 
         last_point = self.PointList[len(self.PointList) - 1]
         # draw all the edges
         for i in range(0, len(self.PointList)):
             current_point = self.PointList[i]
             # adjusted cordinates
+            canvas.create_oval(current_point[0] + MAX_WIDTH/2 +1, (-1) * current_point[1] + MAX_HEIGHT/2 +1, current_point[0] + MAX_WIDTH/2 -1, (-1) * current_point[1] + MAX_HEIGHT/2 -1, fill = "#031402")
             canvas.create_line(current_point[0] + MAX_WIDTH/2, (-1) * current_point[1] + MAX_HEIGHT/2, last_point[0] + MAX_WIDTH/2, (-1) * last_point[1] + MAX_HEIGHT/2, fill = "#32bf28")
             canvas.pack(fill=tk.BOTH, expand=1)
             last_point = current_point
@@ -42,7 +46,7 @@ def GiftWrap(inData):
 
     # if less than a triangle
     if len(inData) < 3:
-        return outData
+        return inData
 
     mainP = inData[0]
     outData = []
@@ -65,10 +69,11 @@ def GiftWrap(inData):
     return outData
 
 def main():
-    # points_list = [(7, 7), (7, -7), (-7, -7), (-7, 7), (9, 0), (-9, 0),
-    #            (0, 9), (0, -9), (0, 0), (1, 2), (-2, 1), (-1, -1),
-    #            (3, 4), (4, 3), (-5, 4), (6, 5)]
-    points_list = [(100,100),(-66,-85),(11,-22)]
+    points_list = [(7, 7), (7, -7), (-7, -7), (-7, 7), (9, 0), (-9, 0),
+               (0, 9), (0, -9), (0, 0), (1, 2), (-2, 1), (-1, -1),
+               (3, 4), (4, 3), (-5, 4), (6, 5)]
+    #points_list = [(100,100),(-66,-85),(11,-22)]
+
     root = tk.Tk()
     ex = Example(GiftWrap(points_list))
     print(ex.PointList)
